b619d4b606bae8c1b1a7f647619d9feddb85c661
temocj <cuauhtemoc.jacobson@gmail.com>
2020-09-01 09:14:32 +0100

    added if-else for customized win messages

--- precourse_recap.py
+++ precourse_recap.py
@@ -1,3 +1,6 @@
+# Copyright 2020 Temoc
+# TODO: add multiplayer betting system
+
 import random
 
 rand_n = int(random.random() * 100)
@@ -8,7 +11,16 @@ upper = rand_n + 1 + int(random.random() * 2)
 answer = input("Guess the random number between " \
     + str(lower) + " and " + str(upper) + ": ")
 
+guesses = 1
+max_guesses = upper - lower - 1
+
 while answer != str(rand_n):
     answer = input("Try again: ")
+    guesses += 1
 
-print("Correct!")
+if guesses == 1:
+    print("Aren't you lucky!")
+elif guesses == max_guesses:
+    print("You finally got it!")
+else:
+    print("Correct!")
